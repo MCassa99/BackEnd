f8b3f5f2aa203094bc724abc77be5979d05c9978
MCassa <m_cassa@icloud.com>
2024-02-16 15:41:12 -0300

    update Clase9 Handlebars

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import productRouter from './routes/productsRoutes.js';
 import cartRouter from './routes/cartRoutes.js';
 import upload from './config/multer.js';
 import { __dirname } from './path.js';
+import { engine } from 'express-handlebars';
 
 //Config
 const app = express();
@@ -11,6 +12,10 @@ const PORT = 3000;
 //Middlewares
 app.use(express.json());
 app.use('/static', express.static(__dirname + '/public'));
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', __dirname + '/views');
+
 
 //Routes
 app.use('/api/products', productRouter);
@@ -23,6 +28,20 @@ app.post('/upload', upload.single('product'), (req, res) => {
     } catch (error) {
         res.status(500).send('Error interno del servidor al subir la imagen');
     }
+});
+
+app.get('/static', (req, res) => {
+    const products = [
+        { name: 'Coca Cola', price: 100, image: './images/prod_CocaCola.webp'},
+        { name: 'Pepsi', price: 150, image: './images/prod_Pepsi.png'},
+        { name: 'Sprite', price: 200, image: './images/prod_Sprite.png'},
+        { name: 'Fanta', price: 250, image: './images/prod_Fanta.webp'}            
+    ]
+    res.render('templates/products', {
+        mostrarProductos: true,
+        productos: products,
+        css: 'products.css'
+    });
 });
 
 //Server
